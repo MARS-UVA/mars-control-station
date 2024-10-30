d0b86c375768f482f77073b029ec7cc8ea5aed8b
Tony <hpm3ep@virginia.edu>
2024-10-30 16:51:39 -0400

    x and y were swapped

--- react-app/src/gamepad/gamepad.js
+++ react-app/src/gamepad/gamepad.js
@@ -35,8 +35,8 @@ function detectBrowser() {
  */
 const getButtonObjectFromGamepad = (gamepad) => {
     return {
-        x: gamepad.buttons[3].value,
-        y: gamepad.buttons[2].value,
+        x: gamepad.buttons[2].value,
+        y: gamepad.buttons[3].value,
         a: gamepad.buttons[0].value,
         b: gamepad.buttons[1].value,
         lt: gamepad.buttons[6].value,
