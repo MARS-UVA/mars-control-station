8ff54ff6bb4f1c558c2b65701256eabae8f39e9a
Eric Paschke <edpaschke@gmail.com>
2024-12-07 14:39:11 -0500

    updated udp server js file

--- server/udp_server.js
+++ server/udp_server.js
@@ -1,23 +1,67 @@
+const fs = require('fs');
 const dgram = require('dgram');
 
 // Constants
 const PORT = 8080;
-const MESSAGE_LENGTH = 100;
+const MESSAGE_LENGTH = 1500;
 
 // Create a UDP socket
 const server = dgram.createSocket('udp4');
+
+let recievedChunks = {};
+let totalChunks = 0;
+
 
 // Event: On server start
 server.on('listening', () => {
     const address = server.address();
     console.log(`Server is listening on ${address.address}:${address.port}`);
+    // Set buffer size
+    server.setRecvBufferSize(MESSAGE_LENGTH);
 });
 
 // Event: On receiving a message
 server.on('message', (message, remote) => {
-    console.log(`Received message from IP: ${remote.address} and port: ${remote.port}`);
-    console.log(`Msg from client: ${message.toString()}`);
+    //console.log(`Received message from IP: ${remote.address} and port: ${remote.port}`);
+    //console.log(`Msg from client: ${message.toString()}`);
 
+    const sequenceNumber = message[0];
+    const totalPackets = (message[1] << 8) | message[2];
+    const chunkData = message.subarray(3);
+    console.log(`Recieved packet ${sequenceNumber + 1} of ${totalPackets}`);
+    recievedChunks[sequenceNumber] = chunkData;
+    totalChunks = totalPackets;
+
+    // Check if all packets have been recieved
+    if(Object.keys(recievedChunks).length == totalChunks) {
+        console.log("All chunks recieved. Reassembling image.")
+        const fullImage = Buffer.concat(Object.values(recievedChunks));
+
+        // Write image to file
+        fs.writeFile('output.jpg', fullImage, (err) => {
+            if(err) {
+                console.error("Error writing file: ", err)
+            } else {
+                console.log("File reassembled and saved successfully (?)")
+            }
+        })
+
+        // Clear chunks to recieve new image
+        recievedChunks = {};
+    }
+
+    /*
+    // output image tetings
+    fs.writeFile('output.jpg', message, (err) => {
+        if (err) {
+            console.error('Error writing the file:', err);
+        } else {
+            console.log('JPEG file created successfully as output.jpg');
+        }
+    });
+    */
+
+    /*
     // Echo the message back to the client
     server.send(message, 0, message.length, remote.port, remote.address, (err) => {
         if (err) {
@@ -26,6 +70,7 @@ server.on('message', (message, remote) => {
             console.log("Message echoed back to client");
         }
     });
+    */
 });
 
 // Bind the server to the port and IP
