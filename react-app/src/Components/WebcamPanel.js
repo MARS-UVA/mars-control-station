ad5a09347c803e70d49e712387ea13628a1ec4d9
Eric Paschke <rgf5pp@virginia.edu>
2025-02-22 16:13:35 -0500

    deleted commenets

--- react-app/src/Components/WebcamPanel.js
+++ react-app/src/Components/WebcamPanel.js
@@ -4,7 +4,6 @@ import { Camera } from "lucide-react";
 // This component renders a panel with a webcam feed (currently showing laptop webcam)
 function WebcamPanel() {
     const [imageSrc, setImageSrc] = useState(null);
-    //const videoRef = useRef(null);
 
     useEffect(() => {
       const ws = new WebSocket("ws://localhost:3001");
@@ -13,20 +12,6 @@ function WebcamPanel() {
         const blob = new Blob([event.data], {type: 'image/jpeg'});
         setImageSrc(URL.createObjectURL(blob));
       }
-      /*      
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-          navigator.mediaDevices
-            .getUserMedia({ video: true })
-            .then((stream) => {
-              if (videoRef.current) {
-                videoRef.current.srcObject = stream;
-              }
-            })
-            .catch((error) => {
-              console.error("Error accessing webcam:", error);
-            });
-        }
-            */
       return() => ws.close();
     }, []);
 
