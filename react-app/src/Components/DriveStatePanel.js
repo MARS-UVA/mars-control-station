bb6d940c3a825b60484a0c0dfb32275534162657
abelsaberhagen <abelsaberhagen@gmail.com>
2025-03-02 15:50:02 -0500

    Update DriveStatePanel.js
    drivestatepanel change

--- react-app/src/Components/DriveStatePanel.js
+++ react-app/src/Components/DriveStatePanel.js
@@ -13,6 +13,14 @@ function DriveStatePanel() {
           {label}
         </button>
       );
+  
+  // Add motor control buttons that will beb shown in Direct Drive Mode
+  const MotorButton = ({ motorNumber }) => (
+  <button className="motor-button">
+     Motor {motorNumber} 
+     </button>)
+
+
   // Render the component UI
   return (
     <div className="panel">
@@ -25,9 +33,30 @@ function DriveStatePanel() {
                 />
               ))}
             </div>
-            <button className="estop-button">
-              ESTOP
+            <button className="estop-stop-button">
+              Motor Stop
             </button>
+            {driveState == "Autonomous Drive" ? <button className="autonomous-stop-button">
+              Autonomous Stop
+            </button> : ""}
+
+            {driveState === 'Direct Drive' && (
+
+              <div className="motor-controls">
+                <h3>Motor Controls</h3> 
+                <div className="motor-grid">
+                  {[1,2,3,4].map((motorNum) => 
+                    (<MotorButton key={motorNum} motorNumbers={motorNum}/>
+                    ))}
+                </div>
+              </div>
+
+            )}
+            
+            
+
+            
+            
           </div>
   );
 }
