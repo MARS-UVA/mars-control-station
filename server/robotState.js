49c09561bb8afd08395d43184cde12ab577cd174
Gorb <srijan.kalla15@gmail.com>
2026-04-05 13:26:28 -0400

    make UI great again

---
+++ server/robotState.js
@@ -0,0 +1,46 @@
+let isPaused = false;
+let isDigging = false;
+let isDumping = false;
+let isStopped = false;
+
+export function getPausedState() {
+    return isPaused;
+}
+export function getDigState() {
+    return isDigging;
+}
+export function getDumpState() {
+    return isDumping;
+}
+export function getStopState() {
+    return isStopped;
+}
+
+export function setPausedState(input) {
+    if (input instanceof Boolean) {
+        isPaused = input;
+    }
+}
+export function setDigState(input) {
+    if (input instanceof Boolean) {
+        isDigging = input;
+    }
+}
+export function setDumpState(input) {
+    if (input instanceof Boolean) {
+        isDumping = input;
+    }
+}
+export function setStopState(input) {
+    if (input instanceof Boolean) {
+        isStopped = input;
+    }
+}
+
+export function getRobotState() {
+    return {
+        pause: isPaused,
+        dig: isDigging,
+        dump: isDumping
+    }
+}
