b70f23b503a54092672bf63d5bd5a222a944d2da
Noodlebirb <ethanvolcanic@gmail.com>
2026-04-10 16:04:47 -0400

    changed ws_server to differentiate between actions and controllers

--- server/ws_server.js
+++ server/ws_server.js
@@ -63,17 +63,20 @@ webSocketServer.on('connection', (ws) => {
                     console.error('error parsing json: ', e);
                     return;
                 }
-                if (data.type === 'uiState') {            
+                if (data.type === 'uiState') {
                     if (data.gamepad2) {
-                        // Handle sending to both esp and jetson
+                        // Handle sending to esp and Jetson
+                        console.log('sending controller to esp');
+                        udpClient.send_controller_jetson(data);
+                        udpClient.send_esp(data);
                     } else if (data.gamepad) {
                         // Handle sending only to jetson
-                        console.log('sending controller to jetson')
+                        console.log('sending controller to jetson');
                         udpClient.send_controller_jetson(data);
                     }
-                }
-                else if (data.type === 'action') {
-                    udpClient.send_action_jetson(data);
+                } else if (data.type === 'action') {
+                    console.log('sending action to jetson: ', data.actionType);
+                    udpClient.send_autonomous_action_jetson(data);
                 }
             } else if (ws === websockets.udpServer) {
                 // websockets.robotFeedback.send(message);
